5e8582074d64745018fe86db9c9e1f6e5818fe16
saifelleuch <sa2014if@gmail.com>
2022-03-23 18:42:19 -0700

    another one

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -12,7 +12,7 @@ int binary_tree_is_root(const binary_tree_t *node)
 	if (node == NULL)
 		return (0);
 
-	if (node->right != NULL && node->left != NULL)
+	if (node->parent == NULL)
 		return (1);
 	else
 		return (0);
