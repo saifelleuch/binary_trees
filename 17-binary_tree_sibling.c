39a8400f18dfe1dad777eca4131dfb59d8a4e0df
saifelleuch <sa2014if@gmail.com>
2022-03-24 15:35:02 -0700

    another one

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - a function that checks if a binary tree is perfect
+ * @tree: is a pointer to the root node of the tree to check
+ * Return: node or NULL
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+
+	if (node == NULL || (node->parent) == NULL)
+		return (NULL);
+
+	if (node == node->parent->right)
+		return (node->parent->left);
+	return (node->parent->right);
+}
